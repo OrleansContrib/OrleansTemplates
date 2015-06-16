d07521953e425a08462095443bec47716e65f01e
nbilal <nbilal@microsoft.com>
2015-06-16 15:15:11 -0700

    Update build script

--- BuildAll.ps1
+++ BuildAll.ps1
@@ -4,10 +4,9 @@ $orleansVersion = "1.0.8"
 msbuild.exe src\ETG.Orleans.sln /t:Clean /p:Configuration=Release
 msbuild.exe src\ETG.Orleans.sln /t:Build /p:Configuration=Release
 
-msbuild.exe test\ETG.Orleans.CodeGenTest.sln /t:Clean /p:Configuration=Release
-msbuild.exe test\ETG.Orleans.CodeGenTest.sln /t:Build /p:Configuration=Release
-
 ### mstest is failing to resolve dependencies; run tests inside visual studio for now.
+#msbuild.exe test\ETG.Orleans.CodeGenTest.sln /t:Clean /p:Configuration=Release
+#msbuild.exe test\ETG.Orleans.CodeGenTest.sln /t:Build /p:Configuration=Release
 #cd test\
 #mstest /testcontainer:ETG.Orleans.CodeGenTest\bin\Release\ETG.Orleans.CodeGenTest.dll
 #cd ..
@@ -22,4 +21,10 @@ msbuild.exe "VSTemplates\VSTemplates.sln" /t:Clean /p:Configuration=Release
 msbuild.exe "VSTemplates\VSTemplates.sln" /t:Build /p:Configuration=Release
 
 Copy-Item "VSTemplates\ETG.Orleans.Templates.VSIX\bin\Release\ETG.Orleans.Templates.VSIX.vsix" .
-Rename-Item ETG.Orleans.Templates.VSIX.vsix ETG.Orleans.Templates.vsix
+
+$vsixName = "ETG.Orleans.Templates.vsix"
+if (Test-Path $vsixName)
+{
+	Remove-Item $vsixName	
+}
+Rename-Item ETG.Orleans.Templates.VSIX.vsix $vsixName
